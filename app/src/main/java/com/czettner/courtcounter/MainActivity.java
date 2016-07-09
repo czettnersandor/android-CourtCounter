a5dae6ecec543ee501b1f033f65982e8808f9c92
Sándor Czettner <sandor@czettner.hu>
2016-07-09 21:10:44 +0100

    Team A updates

--- app/src/main/java/com/czettner/courtcounter/MainActivity.java
+++ app/src/main/java/com/czettner/courtcounter/MainActivity.java
@@ -2,14 +2,31 @@ package com.czettner.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int scoreTeamA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addThreePoints(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoPoints(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOnePoint(View view) {
+        scoreTeamA++;
+        displayForTeamA(scoreTeamA);
     }
 
     /**
